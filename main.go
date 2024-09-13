3a3fa5daa287306fcf9221308a6b4e94675b8a38
Chandan-DK <chandan.dk@nirmata.com>
2024-09-13 17:12:39 +0000

    move jsonParse function to separate library
    Signed-off-by: Chandan-DK <chandan.dk@nirmata.com>

--- main.go
+++ main.go
@@ -1,27 +1,16 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"jsonparsemod/library"
 	"log"
 
 	"github.com/google/cel-go/cel"
-	"github.com/google/cel-go/common/types"
-	"github.com/google/cel-go/common/types/ref"
 )
 
 func main() {
 	env, err := cel.NewEnv(
-		cel.Function("json_parse", // Name by which the function will be called in the CEL expression Example: In this case it would be json_parse(`{}`)
-			// Overload gives us a way to define a function that can be used like this json_parse('{"key": "value"}') instead of <string>.json_parse() which would need to use MemberOverload()
-			cel.Overload("json_parse_string", // ID of the Overload (https://pkg.go.dev/github.com/google/cel-go@v0.21.0/common/decls#OverloadDecl.ID)
-				[]*cel.Type{cel.StringType}, // The argument type our custom function will accept
-				cel.DynType,                 // Return type of our custom function
-				// UnaryBinding takes a function of type UnaryOp which is basically a function that takes a `single`` value and produces an output. (https://pkg.go.dev/github.com/google/cel-go@v0.21.0/common/functions#UnaryOp)
-				// Likewise you also have BinaryBinding and FunctionBinding (https://pkg.go.dev/github.com/google/cel-go@v0.21.0/common/functions#pkg-types)
-				cel.UnaryBinding(jsonParseString),
-			),
-		),
+		library.JsonParseLib(),
 	)
 	if err != nil {
 		log.Fatalf("Environment creation error: %v", err)
@@ -49,40 +38,6 @@ func main() {
 
 	for _, expression := range testCases {
 		helperFuncToEvaluateExpression(env, expression)
-	}
-}
-
-func jsonParseString(val ref.Val) ref.Val {
-	jsonString, ok := val.Value().(string)
-	if !ok {
-		return types.NewErr("expected a string, got %T", val.Type())
-	}
-
-	var parsedJSON any
-	if err := json.Unmarshal([]byte(jsonString), &parsedJSON); err != nil {
-		return types.NewErr("error while parsing JSON: %v", err)
-	}
-
-	return convertToCelValue(parsedJSON)
-}
-
-func convertToCelValue(value any) ref.Val {
-	fmt.Printf("Value Type: %T", value)
-	switch v := value.(type) {
-	case map[string]any:
-		return types.NewStringInterfaceMap(types.DefaultTypeAdapter, v)
-	case []any:
-		return types.NewDynamicList(types.DefaultTypeAdapter, v)
-	case string:
-		return types.String(v)
-	case float64:
-		return types.Double(v)
-	case bool:
-		return types.Bool(v)
-	case nil:
-		return types.NullValue
-	default:
-		return types.NewErr("unsupported JSON type %T", v)
 	}
 }
 
